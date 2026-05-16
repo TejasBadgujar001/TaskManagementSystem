dbdd72a097bddf6ccee21a2a78302a2bbc77443c
TejasBadgujar001 <tejasbbadgujar01@gmail.com>
2026-05-16 20:19:09 +0530

    TaskResponse and TaskRequest DTO created

---
+++ src/main/java/com/Tejas/TaskManagementSystem/DTO/TaskRequest.java
@@ -0,0 +1,23 @@
+package com.Tejas.TaskManagementSystem.DTO;
+
+import com.Tejas.TaskManagementSystem.Entity.UserEntity;
+import com.Tejas.TaskManagementSystem.Enum.Priority;
+import com.Tejas.TaskManagementSystem.Enum.Status;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TaskRequest {
+    private String title;
+    private String description;
+    private Priority priority;
+    private Status status;
+    private LocalDate dueDate;
+    private UserEntity createdBy;
+    private UserEntity assignedUser;
+}
