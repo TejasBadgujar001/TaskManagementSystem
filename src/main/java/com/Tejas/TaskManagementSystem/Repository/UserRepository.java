036223da66a8641e5828a2c7d25705d8052b6c96
TejasBadgujar001 <tejasbbadgujar01@gmail.com>
2026-05-17 20:16:08 +0530

    UserRepository updated , findByName method updated

--- src/main/java/com/Tejas/TaskManagementSystem/Repository/UserRepository.java
+++ src/main/java/com/Tejas/TaskManagementSystem/Repository/UserRepository.java
@@ -4,11 +4,12 @@ import com.Tejas.TaskManagementSystem.Entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
 
-    Optional<UserEntity> findByName(String name);
+    Optional<List<UserEntity>> findByName(String name);
     Optional<UserEntity> findByEmail(String email);
 }
