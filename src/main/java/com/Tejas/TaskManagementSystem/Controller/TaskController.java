94189a1e295149bc297da0a162be6d52c69f5a9c
TejasBadgujar001 <tejasbbadgujar01@gmail.com>
2026-05-18 11:58:46 +0530

    TaskController updated -> add @PreAuthorised to implement RBAC

--- src/main/java/com/Tejas/TaskManagementSystem/Controller/TaskController.java
+++ src/main/java/com/Tejas/TaskManagementSystem/Controller/TaskController.java
@@ -1,15 +1,19 @@
 package com.Tejas.TaskManagementSystem.Controller;
 
+import com.Tejas.TaskManagementSystem.DTO.AuthDto;
 import com.Tejas.TaskManagementSystem.DTO.TaskRequest;
 import com.Tejas.TaskManagementSystem.DTO.TaskResponse;
+import com.Tejas.TaskManagementSystem.DTO.UserResponse;
 import com.Tejas.TaskManagementSystem.Service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.swing.*;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/task")
@@ -19,6 +23,7 @@ public class TaskController {
 
     //API for posting task
     @PostMapping("/post")
+    @PreAuthorize("hasAnyRole('ADMIN','MANAGER')")
     public ResponseEntity<TaskResponse> postTask(@RequestBody TaskRequest request){
         TaskResponse response= service.addTask(request);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
@@ -26,29 +31,34 @@ public class TaskController {
 
     //API for fetching tasks
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<TaskResponse>> getAllTask(){
         List<TaskResponse> responses = service.fetchAllTask();
         return new ResponseEntity<>(responses,HttpStatus.OK);
     }
 
-    @GetMapping("/post")
+    @GetMapping("/postedTask")
+    @PreAuthorize("hasAnyRole('ADMIN','MANAGER')")
     public ResponseEntity<List<TaskResponse>> getAllTaskPostByUser(){
-        List<TaskResponse> responses = service.getAllPostedTaskbyUser();
+        List<TaskResponse> responses = service.getAllPostedTaskByUser();
         return new ResponseEntity<>(responses,HttpStatus.OK);
     }
 
     @GetMapping("/assigned")
+    @PreAuthorize("hasAnyRole('ADMIN','MANAGER','MEMBER')")
     public ResponseEntity<List<TaskResponse>> getAllTaskAssignedToUser(){
         List<TaskResponse> responses = service.getTaskForUser();
         return new ResponseEntity<>(responses,HttpStatus.OK);
     }
     @GetMapping("/{taskId}")
+    @PreAuthorize("hasAnyRole('ADMIN','MANAGER','MEMBER')")
     public ResponseEntity<TaskResponse> getTaskById(@PathVariable Long taskId){
         TaskResponse response = service.getTaskById(taskId);
         return new ResponseEntity<>(response,HttpStatus.OK);
     }
     //API for update task
     @PutMapping("/update/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN','MANAGER')")
     public ResponseEntity<TaskResponse> updateTask(@PathVariable Long id, @RequestBody TaskRequest request){
         TaskResponse response = service.updateTask(id,request);
         return new ResponseEntity<>(response,HttpStatus.OK);
@@ -56,6 +66,7 @@ public class TaskController {
 
     //API for delete task
     @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN','MANAGER')")
     public ResponseEntity<String> deleteTask(@PathVariable Long id){
         String str = service.deleteTask(id);
         return new ResponseEntity<>(str,HttpStatus.OK);
