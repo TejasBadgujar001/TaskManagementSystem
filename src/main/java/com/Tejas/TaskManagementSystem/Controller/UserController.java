01e581205e63ba897f732b02fef70983b80c140e
TejasBadgujar001 <tejasbbadgujar01@gmail.com>
2026-05-18 10:57:09 +0530

    TaskController updated - Make it role based by using @PreAuthorised

--- src/main/java/com/Tejas/TaskManagementSystem/Controller/UserController.java
+++ src/main/java/com/Tejas/TaskManagementSystem/Controller/UserController.java
@@ -7,13 +7,14 @@ import com.Tejas.TaskManagementSystem.Service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
 @RestController()
-@RequestMapping(name = "/user")
+@RequestMapping(path = "/user")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService service;
@@ -26,6 +27,7 @@ public class UserController {
     }
 
     //API for get Users
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/all")
     public ResponseEntity<List<UserResponse>> getAllUsers(){
         List<UserResponse> responseList = service.getAllUsers();
@@ -33,24 +35,28 @@ public class UserController {
     }
 
     @GetMapping("/email/{email}")
+    @PreAuthorize("hasAnyRole('ADMIN','MANAGER','MEMBER')")
     public ResponseEntity<UserResponse> getUserByEmailId(@PathVariable String email){
         UserResponse response = service.getUserByEmail(email);
-        return new  ResponseEntity<>(response,HttpStatus.FOUND);
+        return new  ResponseEntity<>(response,HttpStatus.OK);
     }
 
     @GetMapping("/name/{name}")
-    public ResponseEntity<UserResponse> getUserByName(@PathVariable String name){
-        UserResponse response = service.getUserByName(name);
-        return new  ResponseEntity<>(response,HttpStatus.FOUND);
+    @PreAuthorize("hasAnyRole('ADMIN','MANAGER','MEMBER')")
+    public ResponseEntity<List<UserResponse>> getUserByName(@PathVariable String name){
+        List<UserResponse> response = service.getUserByName(name);
+        return new  ResponseEntity<>(response,HttpStatus.OK);
     }
 
-    @GetMapping("id/{id}")
+    @GetMapping("/id/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN','MANAGER','MEMBER')")
     public ResponseEntity<UserResponse> getUserById(@PathVariable Long id){
         UserResponse response = service.getUserById(id);
-        return new  ResponseEntity<>(response,HttpStatus.FOUND);
+        return new  ResponseEntity<>(response,HttpStatus.OK);
     }
 
     //API for update user profile
+    @PreAuthorize("hasAnyRole('ADMIN','MANAGER','MEMBER')")
     @PutMapping("/update/{id}")
     public ResponseEntity<UserResponse> updateUser(@PathVariable Long id, @RequestBody UserRequest request){
         UserResponse response = service.updateUser(id,request);
@@ -59,13 +65,14 @@ public class UserController {
 
     //API for delete user
     @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<String> deleteUser(@PathVariable Long id){
         String message = service.deleteUser(id);
         return new ResponseEntity<>(message,HttpStatus.OK);
     }
 
     //API for Login
-    @PostMapping("/signin")
+    @PostMapping("/login")
     public ResponseEntity<Map<String,Object>> signIn(@RequestBody AuthDto dto){
         Map<String,Object> res = service.authenticateAndGenerateToken(dto);
         return new ResponseEntity<>(res,HttpStatus.OK);
