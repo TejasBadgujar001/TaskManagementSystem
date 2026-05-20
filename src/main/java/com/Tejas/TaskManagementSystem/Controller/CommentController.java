b93a2509ae289ac53b7c6f3dffc5d253641e664d
TejasBadgujar001 <tejasbbadgujar01@gmail.com>
2026-05-20 18:41:44 +0530

    Comment Controller added

---
+++ src/main/java/com/Tejas/TaskManagementSystem/Controller/CommentController.java
@@ -0,0 +1,57 @@
+package com.Tejas.TaskManagementSystem.Controller;
+
+import com.Tejas.TaskManagementSystem.DTO.CommentRequest;
+import com.Tejas.TaskManagementSystem.DTO.CommentResponse;
+import com.Tejas.TaskManagementSystem.Service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/comment")
+@RequiredArgsConstructor
+public class CommentController {
+    private final CommentService service;
+
+    //API for posting comments
+    @PreAuthorize("hasAnyRole('MANAGER','MEMBER')")
+    @PostMapping("/post/{id}")
+    public ResponseEntity<CommentResponse> addComment(@PathVariable Long id, @RequestBody CommentRequest request){
+        CommentResponse response = service.addComment(id,request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    //API for updating comment
+    @PreAuthorize("hasAnyRole('MANAGER','MEMBER')")
+    @PutMapping("/update/{id}")
+    public ResponseEntity<CommentResponse> updateComment(@PathVariable Long id, @RequestBody CommentRequest request){
+        CommentResponse response = service.updateComment(id,request);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    //API for deleting comment
+    @PreAuthorize("hasAnyRole('MANAGER','MEMBER')")
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteComment(@PathVariable Long id){
+        String response = service.deleteComment(id);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    //API for fetching all comments for task
+    @PreAuthorize("hasAnyRole('ADMIN','MANAGER')")
+    @GetMapping("/task/{id}")
+    public ResponseEntity<List<CommentResponse>> getAllCommentsForTask(@PathVariable Long id){
+        return  new ResponseEntity<>(service.getAllCommentsForTask(id),HttpStatus.OK);
+    }
+    //API for fetching all comments for task
+    @PreAuthorize("hasAnyRole('MEMBER','MANAGER')")
+    @GetMapping("/user/{id}")
+    public ResponseEntity<List<CommentResponse>> getAllCommentsForUser(@PathVariable Long id){
+        return  new ResponseEntity<>(service.getAllCommentsForUser(id),HttpStatus.OK);
+    }
+
+}
